1b4e75125ea5a7b320eae65cabef8f2b5a4772e3
Norm Pierce <npierce.aq2w@dappermapper.com>
2012-09-22 12:21:38 +0100

    Fixed xdgmime support for zero-length files
    (cherry picked from commit 3374e731702628b63af683dfb7a696270d046bd5)

--- ROX-Filer/src/xdgmime.c
+++ ROX-Filer/src/xdgmime.c
@@ -515,6 +515,10 @@ xdg_mime_get_mime_type_for_file (const char  *file_name,
       statbuf = &buf;
     }
 
+  /* ROX: */
+  if (statbuf->st_size == 0)
+    return XDG_MIME_TYPE_EMPTY;
+
   if (!S_ISREG (statbuf->st_mode))
     return XDG_MIME_TYPE_UNKNOWN;
 
